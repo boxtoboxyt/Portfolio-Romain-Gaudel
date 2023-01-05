36196a6df7042aab086cd2f72edc3f495b772396
Romain Gaudel <romaingaudel83@gmail.com>
2023-01-05 18:48:48 +0100

    Mon Portfolio

---
+++ src/components/Portfolio/Data.jsx
@@ -0,0 +1,61 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+import Work6 from "../../assets/work5.jpg";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Oh My Food",
+    category: "web",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Kanap",
+    category: "web",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Piiquante",
+    category: "app",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Kasa",
+    category: "design",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Youtube Clone",
+    category: "web",
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "UX UI interface",
+    category: "design",
+  },
+];
+
+// projects
+export const projectsNav = [
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+  {
+    name: "design",
+  },
+];
---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import "./qualification.css";
+
+const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <section className="qualification section">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">Mon parcours</span>
+
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
+          </div>
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
+          </div>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">OpenClassrooms</h3>
+                <span className="qualification__subtitle">
+                  Développeur Front
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">IUT GEA</h3>
+                <span className="qualification__subtitle">
+                  PACA
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2015 - 2021
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              
+            </div>
+          </div>
+
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Freelance</h3>
+                <span className="qualification__subtitle">
+                  freelance
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2022 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Commercial Tech Bolloré</h3>
+                <span className="qualification__subtitle">
+                  WIFIRST
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2015 - 2021
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Marketing TPE</h3>
+                <span className="qualification__subtitle">ECLS</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2012 - 2014
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
